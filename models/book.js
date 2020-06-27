ea9b5a189cfb7ddff1f5a38ac6a973372f88d408
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-06-27 10:42:56 -0500

    update book model and seeds for project

--- models/book.js
+++ models/book.js
@@ -5,7 +5,9 @@ const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
   synopsis: String,
-  date: { type: Date, default: Date.now }
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  link: { type: String, required: true },
 });
 
 const Book = mongoose.model("Book", bookSchema);
