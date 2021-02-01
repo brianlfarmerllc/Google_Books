733b1312c5d11f522e9adbe1eb9ba12147949e85
brianlfarmerllc <brianlfarmerllc@gmail.com>
2021-01-31 22:40:47 -0600

    updated to MongoDB cloud Atlas

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
+require("dotenv").config();
 const express = require("express");
-
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -15,8 +15,11 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google_books");
+// connecting to mongo locally and with heroku
+mongoose.connect(process.env.MONGODB_URI || process.env.Atlas_Connect, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
 
 // Start the API server
 app.listen(PORT, () => {
